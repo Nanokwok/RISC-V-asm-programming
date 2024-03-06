f5668d2620c859fea5e9446941febbf3dea7d045
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-03-06 13:42:02 +0700

    add mult_recursive

--- mult_recursive.s
+++ mult_recursive.s
@@ -1,33 +1,48 @@
 .text
 main:
-    addi a0, x0, 110            # Set a0 to 110 (a)
-    addi a1, x0, 50             # Set a1 to 50 (b)
-    jal multiply
+    # pass the first argument to a0
+    # pass the second argument to a1
+    addi a0, x0, 120
+    addi a1, x0, 60
+    jal mult
+    
+    # print_int
+    mv a1, a0 # by convention the return value is always in a0
+    addi a0, x0, 1
+    ecall
 
-print:
-    mv a1, a0                   # Move result to a1 (print result)
-    addi a0, x0, 1              # Set a0 to 1 (print integer)
-    ecall                       
-    addi a0, x0, 10             # Set a0 to 10 (program exit)
-    ecall                       
-
-multiply:
-    addi t0, x0, 1              # Set t0 to 1 (base case)
-    beq a1, t0, exit            # If b equals 1, go to exit_base_case
-    addi sp, sp, -4             # Adjust stack pointer
-    sw ra, 0(sp)                # Store return address on stack
-    addi sp, sp, -4             # Adjust stack pointer
-    sw a0, 0(sp)                # Store a0 on stack
-    addi a1, a1, -1             # Decrement b
-    jal multiply                # Recursive call
-    mv t1, a0                   # Store result in t1
-    lw a0, 0(sp)                # Load a0 from stack
-    addi sp, sp, 4              # Restore stack pointer
-    add a0, a0, t1              # Calculate result: a + result_of_recursive_call
-    lw ra, 0(sp)                # Load return address from stack
-    addi sp, sp, 4              # Restore stack pointer
-    jr ra                       # Return
-    add a0, a0, t0              # Add t0 to a0
-
-exit:
-    jr ra                       # Return
+    # exit cleanly
+    addi a0, x0, 10
+    ecall
+    
+mult:
+    # base case
+    # compare a1 with 1, if the two are equal you exit the mult function
+    addi t0, x0, 1
+    beq a1, t0, exit_base_case
+    
+    # recursive case
+    addi sp, sp, -4
+    sw ra, 0(sp) # storing the ra value on to the stack
+    
+    # mult(a, b-1);
+    
+    addi sp, sp, -4
+    sw a0, 0(sp)
+    addi a1, a1, -1
+    jal mult
+    
+    # a + mult(a, b-1)
+    # mv t1, a0
+    
+    # restore the original a value before the call to mult
+    lw t1, 0(sp)
+    addi sp, sp, 4
+    add a0, a0, t1
+    
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    jr ra
+    
+exit_base_case:
+    jr ra
